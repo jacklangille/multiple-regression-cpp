c3178b126de00030f8dae335bbfcedabcebbc179
Jack Langille <jack.w.langille@gmail.com>
2024-06-26 11:29:01 -0300

    remove class

--- regression.cpp
+++ regression.cpp
@@ -3,56 +3,76 @@
 
 using namespace std;
 
-class LinearRegression{
-  private:
-  vector<double> X;
-  vector<vector<double> > mtranspose(const vector<vector<double> >& m){
-      vector<vector<double> > transposed(m[0].size(), vector<double>(m.size()));
-      for(size_t i = 0; i < m.size(); ++i){
-        for(size_t j = 0; j < m[0].size(); ++j){
-          transposed[j][i] = m[i][j];
+vector<vector<double> > MTRANSPOSE(const vector<vector<double> >& M){
+    vector<vector<double> > transposed(M[0].size(), vector<double>(M.size()));
+    for(size_t i = 0; i < M.size(); ++i){
+        for(size_t j = 0; j < M[0].size(); ++j){
+            transposed[j][i] = M[i][j];
         }
-      }
-      return transposed;
     }
-    
-  vector<vector<double> > mmult(const vector<vector<double> >& a, const vector<vector<double> >& b){
-    vector<vector<double> > product(a.size(), vector<double>(b[0].size(), 0));
-    for(size_t i = 0; i < a.size(); ++i){
-      for(size_t j = 0; j < b[0].size(); ++j){
-        for(size_t k = 0; k < b.size(); ++k){
-          product[i][j] += a[i][k] * b[k][j];
+    return transposed;
+}
+
+vector<vector<double> > MMULT(const vector<vector<double> >& A, const vector<vector<double> >& B){
+    vector<vector<double> > product(A.size(), vector<double>(B[0].size(), 0));
+    for(size_t i = 0; i < A.size(); ++i){
+        for(size_t j = 0; j < B[0].size(); ++j){
+            for(size_t k = 0; k < B.size(); ++k){
+                product[i][j] += A[i][k] * B[k][j];
+            }
         }
-      }
     }
     return product;
-  }
-
-
-  // inverse function here
-  
-  void mprint(const vector<vector<double> >& matrix){
-    for (const auto& row : matrix) {
-      for (double value : row) {
-        cout << value << " ";
-      }
-      cout << endl;
+}
+vector<vector<double> > MINVERSE(const vector<vector<double> >& M){
+    int n = M.size();
+    vector<vector<double> > inv(M); // copy initialize inv with M
+    vector<vector<double> > I(n, vector<double>(n,0)); // n rows with n columns filled with 0's
+    
+    // create identity matrix
+    for(int i = 0; i<n;++i)
+        I[i][i] = 1;
+    
+    for(int i = 0; i<n; ++i){
+        double diag = inv[i][i];
+        for(int j = 0; j < n; ++j){
+            inv[i][j] /= diag;
+            I[i][i] /= diag;
+        }
+        for(int k = 0; k < n; ++k){
+            if(i != k){
+                double factor = inv[k][i];
+                for(int j = 0; j < n; ++j){
+                    inv[k][j] -= factor * inv[i][j];
+                    I[k][j] -= factor * I[i][j];
+                }
+            }
+        }
     }
-  }
 
-  public:
-    LinearRegression() {}
-  
-};
+    return I;
 
+}
 
+void MPRINT(const vector<vector<double> >& matrix){
+        for (const auto& row : matrix) {
+            for (double value : row) {
+                cout << value << " ";
+            }
+            cout << endl;
+        }
+    }
 
 
 int main() {
-  vector<vector<double> > A {{1,2},{2,4}};
-  vector<vector<double> > B {{3,1},{2,2}};
+    
+    vector<vector<double> > X {{-1,1.5},{1,-1}};
+    vector<vector<double> > coefficients(X.size(), vector<double>(X.size() , 1));
+
+    MPRINT(coefficients);
+    
 
 
-  return 0;
+    return 0;
 }
 
